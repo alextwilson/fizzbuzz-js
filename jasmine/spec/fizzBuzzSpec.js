60f666b50ce1495f0ad7fcf2c307f008fbb2b0d6
alextwilson <github@alextwilson.co.uk>
2018-01-29 16:52:25 +0000

    Refactor divisible by method to take a divisor

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -9,30 +9,30 @@ describe('Fizzbuzz', function(){
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+      expect(fizzbuzz.isDivisibleBy(3, 3)).toBe(true);
     });
 
     it('divisible by 5', function() {
-      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+      expect(fizzbuzz.isDivisibleBy(5, 10)).toBe(true);
     });
 
     it('divisible by 15', function() {
-      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+      expect(fizzbuzz.isDivisibleBy(15, 30)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function() {
 
     it('divisible by 3', function() {
-      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+      expect(fizzbuzz.isDivisibleBy(3, 1)).toBe(false);
     });
 
     it('divisible by 5', function() {
-      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+      expect(fizzbuzz.isDivisibleBy(5, 1)).toBe(false);
     });
 
     it('divisible by 15', function() {
-      expect(fizzbuzz.isDivisibleByFifteen(1)).toBe(false);
+      expect(fizzbuzz.isDivisibleBy(15, 1)).toBe(false);
     });
   });
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,13 +1,5 @@
 var Fizzbuzz = function() {};
 
-Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 === 0);
-};
-
-Fizzbuzz.prototype.isDivisibleByFive = function(number) {
-  return (number % 5 === 0);
-};
-
-Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
-  return (number % 15 === 0);
+Fizzbuzz.prototype.isDivisibleBy = function(divisor, number) {
+  return (number % divisor === 0);
 };
